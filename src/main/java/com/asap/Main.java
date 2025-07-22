f31912b53adb658ef589b9f488cba62f9b30f028
Maxi <maximaxmaxii690@gmail.com>
2025-07-22 17:32:02 -0300

    first commit

--- src/main/java/com/asap/Main.java
+++ src/main/java/com/asap/Main.java
@@ -1,4 +1,4 @@
-package com.asap-consulting;
+package com.asap;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
